c6cc7013c2a7f61915b0f7a1e64966a806626ed1
Eduardo Sánchez <eduardo@MacBook-Pro-de-Eduardo.local>
2020-01-06 20:55:44 -0600

    Initial commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    template: `
+    <div class="d-flex flex-column align-items-center mt-5 mb-1">
+        <h1>{{ title }}</h1>
+        <p class="lead">Look you favorite beer</p>
+    </div>`
 })
 export class AppComponent {
-  title = 'my-app';
+    title: string = 'We love beer';
 }
